1e647614a25afd0c4f22f7382c9f6cf4de44e665
JHernnz <jh117124@gmail.com>
2017-11-10 10:52:11 -0600

    Update dbclass.py

--- dbclass.py
+++ dbclass.py
@@ -38,4 +38,85 @@ class dbclass(object):
     def editcell(tbl,col,val,idx):
         self.c.execute("UPDATE "+tbl+" SET "+col+"="+val+" WHERE Log="+idx)
         self.db.commit()
+        
+    # Prints a table's header.
+    def header(tbl):
+        self.c.execute("PRAGMA table_info("+tbl+");")
+        rawout = self.c.fetchall()
+        rolen = len(rawout)
+        i = 1
+        refined = "\n[ "
+        while(i < rolen):
+            #refined += rawout[i][1]+" ("+rawout[i][2]+") "
+            # Line above was used for debuging purposes, prints a schema.
+            refined += " |"+rawout[i][1]+"| "
+            i += 1 
+        refined += "]\n"
+        print(refined)
+
+    # Prints out a table.
+    def viewtbl(tbl):
+        # retrieve table/column length
+        self.c.execute("PRAGMA table_info("+tbl+");")
+        rawout = self.c.fetchall()
+        rlen = len(rawout)                  # column length number
+
+        # string with the column names
+        colnames = ""
+        i = 1
+        while(i < rlen):
+            colnames += ","+rawout[i][1]
+            i += 1 
+
+        # initializing list of max column lengths
+        sizes = []
+        i = 0
+        while i < rlen:
+            sizes.append(len(str(rawout[i][1])))
+            i += 1
+
+        # Printing the entire table
+                    
+        # recording max cell space per column
+        result = self.c.execute("SELECT Log"+colnames+" FROM "+tbl)
+        for row in result:
+            idx1 = 0
+            while idx1 < rlen:
+                if(len(str(row[idx1])) > sizes[idx1]):
+                    sizes[idx1] = len(str(row[idx1]))
+                idx1 += 1
+
+        # printing table head
+        buffer = ""
+        self.c.execute("PRAGMA table_info("+tbl+");")
+        i = 0
+        ref = ""
+        while(i < rlen):
+            j = 0
+            while(j < sizes[i]):
+                buffer += " "
+                j += 1
+            ref += "|  "+rawout[i][1]+buffer[len(str(rawout[i][1])):sizes[i]]+"  "
+            i += 1 
+        ref += "|"
+        print(ref)
+
+        # printing table body
+        result = self.c.execute("SELECT Log"+colnames+" FROM "+tbl)             
+        for row in result:
+            idx2 = 0
+            line = ""
+            # handling columns (left to right)
+            while idx2 < rlen:
+                buffer = ""
+                i = 0
+                while(i < sizes[idx2]):
+                    buffer += " "
+                    i += 1
+                        
+                cell = "|  "+str(row[idx2]) + buffer[len(str(row[idx2])):sizes[idx2]] + "  "
+                line += cell
+                idx2 += 1
+            print(line+"|")
+    
 
